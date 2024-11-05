b4d45b46ece32fddd58fbff272ed0070b78350c6
estherlopezdam <estherlopezdam@gmail.com>
2024-11-05 19:08:42 +0100

    añadido controls Y mas

--- assets/src/game.js
+++ assets/src/game.js
@@ -653,77 +653,85 @@ class Game {
                 break;
         }
     }  
+    // handleCollisionWithGhost(collideGhost) {
+    //     if (!this.powerPelletActive) {
+    //         // Resetear la posición de Pac-Man
+    //         this.pacman = new Pacman(this.ctx);
+    //         this.looseLive(); // Pac-Man pierde una vida si no está el PowerPellet activo
+    //     } else {
+    //         // Si el PowerPellet está activo, el fantasma es comido
+    //         collideGhost.eaten = true;
+    //         this.score += 100;
+    
+    //         // Reiniciar las propiedades del fantasma después de 5 segundos
+    //         setTimeout(() => {
+    //             this.resetGhostProperties(collideGhost);
+    //         }, 5000); // Esperar 5 segundos antes de que el fantasma reaparezca
+    //     }
+    // }
+    
+    // // Método para reiniciar las propiedades del fantasma
+    // resetGhostProperties(ghost) {
+    //     switch (ghost.name) {
+    //         case 'blinky':
+    //             ghost.x = 11 * 20; // Cambia estas posiciones según sea necesario
+    //             ghost.y = 14 * 20;
+    //             break;
+    //         case 'pinky':
+    //             ghost.x = 11 * 20;
+    //             ghost.y = 15 * 20;
+    //             break;
+    //         case 'inky':
+    //             ghost.x = 12 * 20;
+    //             ghost.y = 15 * 20;
+    //             break;
+    //         case 'clyde':
+    //             ghost.x = 11 * 20;
+    //             ghost.y = 16 * 20;
+    //             break;
+    //     }
+    //     ghost.vx = 1; // Reiniciar la velocidad
+    //     ghost.vy = 0; // Reiniciar la velocidad
+    //     ghost.currentDirection = 'UP'; // Reiniciar la dirección
+    //     ghost.eaten = false; // El fantasma ya no está comido
+    // }
+    
+        
     handleCollisionWithGhost(collideGhost) {
-        if (!this.powerPelletActive) {
-            // Resetear la posición de Pac-Man
-            this.pacman = new Pacman(this.ctx);
-            this.looseLive(); // Pac-Man pierde una vida si no está el PowerPellet activo
+
+        if(!this.powerPelletActive) {
+              // Reset the Pac-Man's position
+         // Resetear la posición de Pac-Man
+         this.pacman = new Pacman(this.ctx);
+         this.looseLive(); // Pac-Man pierde una vida si no está el PowerPellet activo
+
         } else {
             // Si el PowerPellet está activo, el fantasma es comido
             collideGhost.eaten = true;
             this.score += 100;
-    
-            // Reiniciar las propiedades del fantasma después de 5 segundos
-            setTimeout(() => {
-                this.resetGhostProperties(collideGhost);
-            }, 5000); // Esperar 5 segundos antes de que el fantasma reaparezca
         }
-    }
+         
+ this.ghosts = this.ghosts.map(ghost => {
+     if (ghost === collideGhost) {
+         switch (ghost.name) {
+             case 'blinky':
+                 return new Blinky(this.ctx, this.level);
+             case 'pinky':
+                 return new Pinky(this.ctx, this.level);
+             case 'inky':
+                 return new Inky(this.ctx, this.level);
+             case 'clyde':
+                 return new Clyde(this.ctx, this.level);
+             default:
+                 return ghost;
+         }
+     }
+     return ghost; 
+ });
     
-    // Método para reiniciar las propiedades del fantasma
-    resetGhostProperties(ghost) {
-        switch (ghost.name) {
-            case 'blinky':
-                ghost.x = 11 * 20; // Cambia estas posiciones según sea necesario
-                ghost.y = 14 * 20;
-                break;
-            case 'pinky':
-                ghost.x = 11 * 20;
-                ghost.y = 15 * 20;
-                break;
-            case 'inky':
-                ghost.x = 12 * 20;
-                ghost.y = 15 * 20;
-                break;
-            case 'clyde':
-                ghost.x = 11 * 20;
-                ghost.y = 16 * 20;
-                break;
-        }
-        ghost.vx = 1; // Reiniciar la velocidad
-        ghost.vy = 0; // Reiniciar la velocidad
-        ghost.currentDirection = 'UP'; // Reiniciar la dirección
-        ghost.eaten = false; // El fantasma ya no está comido
+
+     
     }
-    
-        
-    // handleCollisionWithGhost(collideGhost) {
-
-    //     if(!this.powerPelletActive) {
-    //           // Reset the Pac-Man's position
-    //     this.pacman = new Pacman(this.ctx);
-
-    //     }
-
-    //     // Reset the position of the ghost involved in the collision
-    //     this.ghosts = this.ghosts.map(ghost => {
-    //         if (ghost === collideGhost) {
-    //             switch (ghost.name) {
-    //                 case 'blinky':
-    //                     return new Blinky(this.ctx, this.level);
-    //                 case 'pinky':
-    //                     return new Pinky(this.ctx, this.level);
-    //                 case 'inky':
-    //                     return new Inky(this.ctx, this.level);
-    //                 case 'clyde':
-    //                     return new Clyde(this.ctx, this.level);
-    //                 default:
-    //                     return ghost;
-    //             }
-    //         }
-    //         return ghost; 
-    //     });
-    // }
 
     initializeLives() {
         // Create the lives container if it doesn't exist
